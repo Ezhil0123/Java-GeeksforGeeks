4439facb01d7d046c35c438f4af6ab812317cdd1
Ezhil0123 <150767920+Ezhil0123@users.noreply.github.com>
2026-03-22 12:35:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int orangesRot(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                } else if (mat[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+        if (fresh == 0) return 0;
+        int time = 0;
+        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+        while (!q.isEmpty()) {
+            int size = q.size();
+            boolean rotted = false;
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                for (int[] d : dirs) {
+                    int ni = curr[0] + d[0];
+                    int nj = curr[1] + d[1];
+                    if (ni >= 0 && nj >= 0 && ni < n && nj < m && mat[ni][nj] == 1) {
+                        mat[ni][nj] = 2;
+                        q.offer(new int[]{ni, nj});
+                        fresh--;
+                        rotted = true;
+                    }
+                }
+            }
+            if (rotted) time++;
+        }
+        
+        return fresh == 0 ? time : -1;
+    }
+}
