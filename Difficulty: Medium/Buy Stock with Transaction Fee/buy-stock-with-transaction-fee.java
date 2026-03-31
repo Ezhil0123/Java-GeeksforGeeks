bae7881e628dfbe2f3b419370ffd0f7c2da8b709
Ezhil0123 <150767920+Ezhil0123@users.noreply.github.com>
2026-03-31 09:52:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Buy Stock with Transaction Fee/buy-stock-with-transaction-fee.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int arr[], int k) {
+        int n = arr.length;
+        int cash = 0;         
+        int hold = -arr[0];   
+        for (int i = 1; i < n; i++) {
+            cash = Math.max(cash, hold + arr[i] - k);
+            hold = Math.max(hold, cash - arr[i]);     
+        }
+        return cash;
+    }
+}
